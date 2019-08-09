b0d1573ff8548c8857058f6405cc16035079293f
Rajal <rajal.excelsior@gmail.com>
2019-08-09 10:51:30 +0530

    rajal

---
+++ app/src/main/java/test/practical/com/androidprojectsstructuredemo/Activity/FreshOHub_SplashActivity.java
@@ -0,0 +1,16 @@
+package test.practical.com.androidprojectsstructuredemo.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import test.practical.com.androidprojectsstructuredemo.R;
+
+public class FreshOHub_SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fresh_ohub__splash);
+    }
+}
