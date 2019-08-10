7de4934958f50b81791efd2f932003cf694bb1a0
Rajal <rajal.excelsior@gmail.com>
2019-08-10 10:57:53 +0530

    rajal

---
+++ app/src/main/java/test/practical/com/androidprojectsstructuredemo/Activity/FreshOhub_ShareItActivity.java
@@ -0,0 +1,16 @@
+package test.practical.com.androidprojectsstructuredemo.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import test.practical.com.androidprojectsstructuredemo.R;
+
+public class FreshOhub_ShareItActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fresh_ohub__share_it);
+    }
+}
